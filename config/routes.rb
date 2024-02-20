2e51e9fb8ec5735f13726d269db2a6d92f9cec72
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-02-20 15:24:45 +0100

    rails routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-
-  
-
+  resources :pages
 end
+
+# rails routes: 
+# pages       GET       /pages(.:format)                       pages#index
+#             POST      /pages(.:format)                       pages#create
+# new_page    GET       /pages/new(.:format)                   pages#new
+# edit_page   GET       /pages/:id/edit(.:format)              pages#edit
+# page        GET       /pages/:id(.:format)                   pages#show
+#             PATCH     /pages/:id(.:format)                   pages#update
+#             PUT       /pages/:id(.:format)                   pages#update
+#             DELETE    /pages/:id(.:format)                   pages#destroy
