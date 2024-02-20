df7cb252f9fbf265bc13f280dabd0275667a629c
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-02-20 15:21:42 +0100

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+10.times do |i|
+  Article.create(title: Faker::Music.band, content: Faker::Sports::Football.player)
+end
